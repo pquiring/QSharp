702bcd680b63570f884f38014a76f75cc29c5cbd
Peter Quiring <pquiring@gmail.com>
2017-10-31 21:45:51 -0400

    add Qt.Core.List generic

--- classlib/Qt/Core/List.cs
+++ classlib/Qt/Core/List.cs
@@ -2,10 +2,10 @@ using Qt.QSharp;
 
 namespace Qt.Core {
     [CPPClass(
-        "private: QVector<T> *$q;\r\n" + 
-        "public: Array() {$q = new QVector<T>();}\r\n"
+        "private: QList<T> *$q;\r\n" + 
+        "public: List() {$q = new QList<T>();}\r\n"
     )]
-    public class Array<T> {
+    public class List<T> {
         public void Add(T t) {
             CPP.Add("$q->append(t);");
         }
@@ -13,12 +13,12 @@ namespace Qt.Core {
             CPP.Add("$q->insert(idx, t);");
         }
         public T Get(int idx) {
-            return (T)CPP.ReturnObject("$q->at(idx)");
+            return (T)CPP.ReturnObject("$q->value(idx)");
         }
         public int Size() {return CPP.ReturnInt("$q->size()");}
         public bool Contains(T t) {return CPP.ReturnBool("$q->contains(t)");}
         public void Remove(int idx) {CPP.Add("$q->removeAt(idx);");}
-        ~Array() {
+        ~List() {
             CPP.Add("delete $q;");
         }
     }
