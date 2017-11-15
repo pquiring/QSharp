eff17069b173f3e3800f4599be62a758e2e0a2ff
Peter Quiring <pquiring@gmail.com>
2017-11-15 15:31:12 -0500

    AbstractSocket.Bind() port has default of 0

--- classlib/Qt/Network/AbstractSocket.cs
+++ classlib/Qt/Network/AbstractSocket.cs
@@ -8,7 +8,7 @@ namespace Qt.Network {
     )]
     public class AbstractSocket : IODevice {
         public void Abort() {CPP.Add("$q->abort();");}
-        public void Bind(int port) {CPP.Add("$q->bind(port);");}
+        public void Bind(int port = 0) {CPP.Add("$q->bind(port);");}
         public void Connect(String host, int port) {CPP.Add("$q->connectToHost(host->qstring(), port);");}
         public void Disconnect() {CPP.Add("$q->disconnectFromHost();");}
         public bool WaitForConnected(int msec = 30000) {return CPP.ReturnBool("$q->waitForConnected(msec)");}
