58bcfb138959d6c40f8b03cb64d6c810a26876e2
Peter Quiring <pquiring@gmail.com>
2017-12-06 14:37:20 -0500

    add ByteArray functions

--- classlib/Qt/Core/ByteArray.cs
+++ classlib/Qt/Core/ByteArray.cs
@@ -20,11 +20,30 @@ namespace Qt.Core {
             CPP.Add("$checkArray(buf, offset, length);");
             CPP.Add("$q->append((const char*)(buf->data() + offset), length);");
         }
+        public void Insert(int index, byte[] buf, int offset, int length) {
+            CPP.Add("$checkArray(buf, offset, length);");
+            CPP.Add("$q->insert(index, (const char*)(buf->data() + offset), length);");
+        }
+        public void Remove(int pos, int length) {
+            CPP.Add("$q->remove(pos, length);");
+        }
         public byte[] ToArray() {
             CPP.Add("int length = $q->size();");
             CPP.Add("std::shared_ptr<QSharpArray<uint8>> array = std::make_shared<QSharpArray<uint8>>(length);");
             CPP.Add("std::memcpy(array->data(), $q->data(), length);");
             return (byte[])CPP.ReturnObject("array");
+        }
+        public void Clear() {
+            CPP.Add("$q->clear();");
+        }
+        public void SetSize(int size) {
+            CPP.Add("$q->resize(size);");
+        }
+        public ByteArray ToBase64() {
+            return (ByteArray)CPP.ReturnObject("std::make_shared<ByteArray>($q->toBase64())");
+        }
+        public static ByteArray FromBase64(ByteArray base64) {
+            return (ByteArray)CPP.ReturnObject("std::make_shared<ByteArray>(QByteArray::fromBase64(*base64->$q))");
         }
         public new String ToString() {
             return new String(this);
