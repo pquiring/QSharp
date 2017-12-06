313ad128dd6edbf4b5b205660441e46ad93f1d19
Peter Quiring <pquiring@gmail.com>
2017-12-06 14:01:46 -0500

    remove paint under/over GL functions

--- classlib/Qt/Gui/OpenGLWidget.cs
+++ classlib/Qt/Gui/OpenGLWidget.cs
@@ -6,8 +6,6 @@ namespace Qt.Gui {
     [CPPClass(
         "public: void initializeGL() {InitializeGL();}" +
         "public: void paintGL() {PaintGL();}" +
-        "public: void paintOverGL() {PaintOverGL();}" +
-        "public: void paintUnderGL() {PaintUnderGL();}" +
         "public: void resizeGL(int x, int y) {ResizeGL(x, y);}"
     )]
     public class OpenGLWidget : Widget {
@@ -18,8 +16,6 @@ namespace Qt.Gui {
         public virtual void InitializeGL() { }
         /** This function is called to print the window.  This is where gl..() functions should be called. */
         public virtual void PaintGL() { }
-        public virtual void PaintOverGL() { }
-        public virtual void PaintUnderGL() { }
         /** This function is called when the window is resized. */
         public virtual void ResizeGL(int w, int h) { }
 
