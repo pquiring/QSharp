ac78c442f2224994358cd58c0a5d6685c0205ffa
Peter Quiring <pquiring@gmail.com>
2018-03-07 13:51:00 -0500

    add Float / Double <-> Int / Long conversion functions

--- classlib/Qt/Core/Double.cs
+++ classlib/Qt/Core/Double.cs
@@ -8,5 +8,19 @@ namespace Qt.Core {
         public static double ValueOf(String s) {
             return CPP.ReturnDouble("$check(s)->$value()->toDouble()");
         }
+        public static double LongBitsToDouble(long bits) {
+            double value = 0f;
+            CPP.Add("union {int64 _bits; double _double;};");
+            CPP.Add("_bits = bits;");
+            CPP.Add("value = _double;");
+            return value;
+        }
+        public static long DoubleToLongBits(double value) {
+            long bits = 0;
+            CPP.Add("union {int64 _bits; double _double;};");
+            CPP.Add("_double = value;");
+            CPP.Add("bits = _bits;");
+            return bits;
+        }
     }
 }
--- classlib/Qt/Core/Float.cs
+++ classlib/Qt/Core/Float.cs
@@ -8,5 +8,19 @@ namespace Qt.Core {
         public static float ValueOf(String s) {
             return CPP.ReturnFloat("$check(s)->$value()->toFloat()");
         }
+        public static float IntBitsToFloat(int bits) {
+            float value = 0f;
+            CPP.Add("union {int32 _bits; float _float;};");
+            CPP.Add("_bits = bits;");
+            CPP.Add("value = _float;");
+            return value;
+        }
+        public static int FloatToIntBits(float value) {
+            int bits = 0;
+            CPP.Add("union {int32 _bits; float _float;};");
+            CPP.Add("_float = value;");
+            CPP.Add("bits = _bits;");
+            return bits;
+        }
     }
 }
