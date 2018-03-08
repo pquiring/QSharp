e0bb9d42c20829207c2ce835e03a762ca21606c7
Peter Quiring <pquiring@gmail.com>
2018-03-07 20:41:08 -0500

    add ByteArrayStream(ByteArray) ctor

--- classlib/Qt/Core/ByteArrayStream.cs
+++ classlib/Qt/Core/ByteArrayStream.cs
@@ -7,6 +7,11 @@ namespace Qt.Core {
             CPP.Add("$q = std::make_shared<QBuffer>();");
             CPP.Add("$base((std::shared_ptr<QIODevice>)$q);");
         }
+        public ByteArrayStream(ByteArray data) {
+            CPP.Add("$q = std::make_shared<QBuffer>();");
+            CPP.Add("$base((std::shared_ptr<QIODevice>)$q);");
+            SetData(data);
+        }
         public void SetData(ByteArray data) {
             CPP.Add("$q->setBuffer($check(data)->$value());");
         }
