e082f8b8ec0e459aeccd138d848502f415453cb3
Peter Quiring <pquiring@gmail.com>
2018-03-11 18:20:09 -0400

    simplify Matrix4x4 SetAxisAngle()

--- classlib/Qt/Gui/Matrix4x4.cs
+++ classlib/Qt/Gui/Matrix4x4.cs
@@ -100,83 +100,8 @@ namespace Qt.Gui {
         }
         /** Set matrix using axis-angle(vector) (see http://en.wikipedia.org/wiki/Axis_angle) */
         public void SetAxisAngle(float angle, float x, float y, float z) {
-            float xx, yy, zz, xy, yz, zx, xs, ys, zs, one_c, s, c;
-
-            s = (float)Math.Sin( angle * (float)Math.PI / 180.0f );
-            c = (float)Math.Cos( angle * (float)Math.PI / 180.0f );
-
             SetIdentity();
-
-            if (x == 0.0f) {
-                if (y == 0.0f) {
-                    if (z != 0.0f) {
-                        /* rotate only around z-axis */
-                        m[0+0*4] = c;
-                        m[1+1*4] = c;
-                        if (z < 0.0) {
-                            m[0+1*4] = s;
-                            m[1+0*4] = -s;
-                        } else {
-                            m[0+1*4] = -s;
-                            m[1+0*4] = s;
-                        }
-                        return;
-                    }
-                } else if (z == 0.0f) {
-                    /* rotate only around y-axis */
-                    m[0+0*4] = c;
-                    m[2+2*4] = c;
-                    if (y < 0.0) {
-                        m[0+2*4] = -s;
-                        m[2+0*4] = s;
-                    } else {
-                        m[0+2*4] = s;
-                        m[2+0*4] = -s;
-                    }
-                    return;
-                }
-            } else if (y == 0.0f) {
-                if (z == 0.0f) {
-                    /* rotate only around x-axis */
-                    m[1+1*4] = c;
-                    m[2+2*4] = c;
-                    if (x < 0.0) {
-                        m[1+2*4] = s;
-                        m[2+1*4] = -s;
-                    } else {
-                        m[1+2*4] = -s;
-                        m[2+1*4] = s;
-                    }
-                    return;
-                }
-            }
-
-            float mag;
-            //complex rotation
-            mag = (float)Math.Sqrt(x * x + y * y + z * z);
-            if (mag <= 1.0e-4f) return;        //rotation too small
-            x /= mag;
-            y /= mag;
-            z /= mag;
-            xx = x * x;
-            yy = y * y;
-            zz = z * z;
-            xy = x * y;
-            yz = y * z;
-            zx = z * x;
-            xs = x * s;
-            ys = y * s;
-            zs = z * s;
-            one_c = 1.0f - c;
-            m[0+0*4] = (one_c * xx) + c;
-            m[0+1*4] = (one_c * xy) - zs;
-            m[0+2*4] = (one_c * zx) + ys;
-            m[1+0*4] = (one_c * xy) + zs;
-            m[1+1*4] = (one_c * yy) + c;
-            m[1+2*4] = (one_c * yz) - xs;
-            m[2+0*4] = (one_c * zx) - ys;
-            m[2+1*4] = (one_c * yz) + xs;
-            m[2+2*4] = (one_c * zz) + c;
+            Rotate(angle, x, y, z);
         }
         /** this = this * other */
         public void Multiply4x4(Matrix4x4 other) {
