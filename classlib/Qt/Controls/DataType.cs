ebd3c76dee391f8839acafff6ba917a6cbe23ca4
Peter Quiring <pquiring@gmail.com>
2018-03-15 18:39:38 -0400

    add Qt Controls DataType

---
+++ classlib/Qt/Controls/DataType.cs
@@ -0,0 +1,6 @@
+namespace Qt.Controls {
+    /** Data types for Write() function.    Only AB protocol requires these. */
+    public enum DataType {
+        ANY, INTEGER16, INTEGER32, FLOAT, BOOLEAN
+    }
+}
